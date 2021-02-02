6f89d0b4d124a3f99d758e321956053bab4e1ddc
David <davidpineda.co@gmail.com>
2021-02-02 10:58:59 +0000

    initial commit

---
+++ android/app/src/main/kotlin/com/devnco/bmi_calculator_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.devnco.bmi_calculator_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BmiApp());
+
+class BmiApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
