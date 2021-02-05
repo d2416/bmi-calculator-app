cf61fc6d74d44ec0a182bb3132dc357d32ac7208
David <davidpineda.co@gmail.com>
2021-02-05 13:18:29 +0000

    move calculate button to own file

---
+++ lib/calculate_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class CalculateButton extends StatelessWidget {
+  CalculateButton({@required this.onPressed, @required this.label});
+
+  final String label;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        child: Center(
+          child: Text(
+            label,
+            style: kCalculateButtonTextStyle,
+          ),
+        ),
+        color: kBottomContainerColor,
+        margin: EdgeInsets.only(top: 10.0),
+        padding: EdgeInsets.only(bottom: 20.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+      ),
+    );
+  }
+}
