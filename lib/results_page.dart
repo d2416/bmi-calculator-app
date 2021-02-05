e87d35d4cf2c7a9cc53729df68a013d306496eb2
David <davidpineda.co@gmail.com>
2021-02-05 13:20:24 +0000

    create result page

---
+++ lib/results_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'reusable_card.dart';
+import 'calculate_button.dart';
+
+class ResultsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Your Result',
+          style: TextStyle(fontSize: 35.0),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              color: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Normal',
+                    style: kResultLabelStyle,
+                  ),
+                  Text(
+                    '20',
+                    style: kResultValueStyle,
+                  ),
+                  Text(
+                    'Good job!',
+                    style: kResultResultText,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          CalculateButton(
+            onPressed: () {
+              Navigator.pop(context, '/');
+            },
+            label: 'RE-CALCULATE',
+          ),
+        ],
+      ),
+    );
+  }
+}
